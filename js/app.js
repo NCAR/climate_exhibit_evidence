6fd77818eac0a2a1f8d8bd9d1af0d65736c679e4
Sharon Clark <sclark@ucar.edu>
2016-05-24 11:38:59 -0600

    IIFE and named functions

--- js/app.js
+++ js/app.js
@@ -1,43 +1,42 @@
-angular.module("edu.ucar.scied.evidence", [
-    "edu.ucar.scied.controllers", 
+(function () {
+    'use strict';
+    angular.module("edu.ucar.scied.evidence", [
+    "edu.ucar.scied.controllers",
     "edu.ucar.scied.controllers.videos",
     "edu.ucar.scied.controllers.evidence",
     "edu.ucar.scied.controllers.evidence.model_it",
     "edu.ucar.scied.services",
     "edu.ucar.scied.directives",
     "edu.ucar.scied.filters",
-    "ngRoute",  
-    "ngMaterial",    
+    "ngRoute",
+    "ngMaterial",
     "com.2fdevs.videogular",
     "com.2fdevs.videogular.plugins.controls",
     "com.2fdevs.videogular.plugins.overlayplay",
     "com.2fdevs.videogular.plugins.poster",
-    "angulartics", 
+    "angulartics",
     "angulartics.google.analytics"
-                          ]).
-config(["$routeProvider", function($routeProvider) {
-  $routeProvider.
-	when("/", 
-         {
-            templateUrl: "/core/templates/menu_grid.html", 
+    ]).
+    config(["$routeProvider", function ($routeProvider) {
+        $routeProvider.
+        when("/", {
+            templateUrl: "/core/templates/menu_grid.html",
             controller: "homeCtrl",
-        }
-    ).
-   when("/videos", 
-         {
-            templateUrl: "/core/templates/menu_grid.html", 
+        }).
+        when("/videos", {
+            templateUrl: "/core/templates/menu_grid.html",
             controller: "videosCtrl",
-        }
-    ).
-    when("/videos/:videoId", 
-         {
-            templateUrl: "/core/templates/video_player.html", 
+        }).
+        when("/videos/:videoId", {
+            templateUrl: "/core/templates/video_player.html",
             controller: "playerCtrl"
-        }
-    ).  
-    when("/apps/model-it", {
-        templateUrl: "templates/model_it.html",
-        controller: "modelItCtrl"
-    }).
-	otherwise({redirectTo: '/'});
-}]);
+        }).
+        when("/apps/model-it", {
+            templateUrl: "templates/model_it.html",
+            controller: "modelItCtrl"
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }]);
+})();
--- js/controllers/controllers.evidence.js
+++ js/controllers/controllers.evidence.js
@@ -1,24 +1,33 @@
-angular.module('edu.ucar.scied.controllers.evidence', []).
-controller('homeCtrl', function ($rootScope, $scope) {
-    $rootScope.showFooter = false;
-    $rootScope.bodylayout = 'home';
-    $rootScope.menulist = 'home';
-    $scope.cols = 3;
-    $scope.data = 'data/menu_main.json';
-})
-.controller('videosCtrl', function ($rootScope, $scope, ContentData) {
-    $rootScope.showFooter = true;
-    $rootScope.bodylayout = 'videos';
-    $rootScope.menulist = 'videos';
-    $scope.pagetitle = "Videos";
-    $scope.header_class = "larger";
-    $scope.cols = 3;
-    $scope.data = 'data/menu_main.json';
-})
-.controller('playerCtrl', function($rootScope,$scope){
-    $rootScope.showFooter = true;
-    $rootScope.bodylayout = 'video-player';  
-    $scope.backButton = true;
-    $scope.backButtonText = "Videos";
-    $scope.backPage = "#/videos";
-});
+(function () {
+    'use strict';
+    angular.module('edu.ucar.scied.controllers.evidence', [])
+        .controller('homeCtrl', homeCtrl)
+        .controller('videosCtrl', videosCtrl)
+        .controller('playerCtrl', playerCtrl);
+
+    function homeCtrl($rootScope, $scope) {
+        $rootScope.showFooter = false;
+        $rootScope.bodylayout = 'home';
+        $rootScope.menulist = 'home';
+        $scope.cols = 3;
+        $scope.data = 'data/menu_main.json';
+    }
+
+    function videosCtrl($rootScope, $scope, ContentData) {
+        $rootScope.showFooter = true;
+        $rootScope.bodylayout = 'videos';
+        $rootScope.menulist = 'videos';
+        $scope.pagetitle = "Videos";
+        $scope.header_class = "larger";
+        $scope.cols = 3;
+        $scope.data = 'data/menu_main.json';
+    }
+
+    function playerCtrl($rootScope, $scope) {
+        $rootScope.showFooter = true;
+        $rootScope.bodylayout = 'video-player';
+        $scope.backButton = true;
+        $scope.backButtonText = "Videos";
+        $scope.backPage = "#/videos";
+    };
+})();
